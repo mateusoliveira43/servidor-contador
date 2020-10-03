d16320b907e882e363379cabf9be0b1dfb05071c
Mateus Oliveira <66965232+mateusoliveira43@users.noreply.github.com>
2020-10-03 10:45:36 -0300

    Develop (#5)
    * começo da implementacao do servidor
    
    * termino da implementacao do servidor
    
    * atualizando arquivo README
    
    * implentaçao de mensagens de erro explicativas ao usuario e começo da implemntacao dos testes de resposta do servidor
    
    * atualizando arquivo README
    
    * fazendo os testes do servidor funcionarem
    
    * abilitando uso de docker
    
    * corrigindo erros no arquivo README
    
    * arrumando problema para rodar com docker
    
    * comando para rodar testes com o docker
    
    * correçao de erros no algoritmo e dockerfile
    
    * revisao final do codigo
    
    * correçao de erro no arquivo routes.js

--- routes.js
+++ routes.js
@@ -2,8 +2,8 @@ import express from 'express';
 import retornaRespostaIndex from './controllers/indexControler';
 import retornaRespostaServidor from './controllers/servidorController';
 
-export default routes;
 const routes = express.Router();
+export default routes;
 
 routes.get('/', retornaRespostaIndex);
 routes.get('/:numero', retornaRespostaServidor);
